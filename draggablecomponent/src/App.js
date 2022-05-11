ae0ea853cb9831cb92f728bdb9bf856595658566
Dzeko <vivala0519@naver.com>
2022-05-11 21:39:28 +0900

    [feat] draggable component

--- draggablecomponent/src/App.js
+++ draggablecomponent/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import Draggable from './components/Draggable';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default function App() {
+    return (
+    <Page>
+        <Draggable/>
+    </Page>
+    )
 }
 
-export default App;
+const Page = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    background-color: black;
+    width: 1000px;
+    height: 1000px;
+`
